b6a5e5a380d08503ff3368ab78cd5755277a100a
prayash231 <138357568+prayash231@users.noreply.github.com>
2024-09-19 17:15:07 +0600

    amount form added in home.js file

--- JS/login.js
+++ JS/login.js
@@ -8,9 +8,21 @@
 document.getElementById("btn-login").addEventListener("click", function(event){
     // step-2: prevent deafult behavior (prevent reloading browser when clicked button inside form tag)
     event.preventDefault(); //vejailla to beginners
-    console.log("Login Button clicked");
+    // console.log("Login Button clicked");
 
-    // step-3: get the phone number
+    // step-3: get the phone number and pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+
+    // step-4: validate phone or pin
+    // This is not an ideal way for validation. This is temporary system for my project
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        // console.log("you are logged in");
+        // step-5: allow user to use the website
+        window.location.href = '/home.html';
+    }
+    else{
+        alert("wrong phone number or pin");
+    }
 });
